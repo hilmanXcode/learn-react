8886faf8643f15de3c7707f79e008ae7bcfa89aa
hilmanXcode <hilmanxcode@gmail.com>
2023-11-02 15:30:11 +0700

    (learn): useRef

--- src/components/homepages.jsx
+++ src/components/homepages.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Profile from "./profile";
 import Introduction from "./introduction";
@@ -6,6 +6,7 @@ import Introduction from "./introduction";
 function HomePages(){
 
     const navigate = useNavigate();
+    const linkRef = useRef(null);
 
     const [getProfileVal, setProfileVal] = useState("");
     const [pacarSaya, setPacarSaya] = useState(0);
@@ -16,6 +17,14 @@ function HomePages(){
         setProfileVal(inputval);
     }
 
+    const goto = (ref) => {
+        window.scrollTo({
+            top: ref.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
     // when web is load for the first time
     useEffect(() => {
         console.log("Web Ke Load");
@@ -24,54 +33,19 @@ function HomePages(){
     // when data is change
     useEffect(() => {
         if(pacarSaya === 1){
-            setNamaPacar("JODI")
+            setNamaPacar("IKAN")
         } else {
-            setNamaPacar("Uhuy")
+            setNamaPacar("ZAI")
         }
     }, [pacarSaya]);
 
     return(
         <>
+            <button onClick={() => goto(linkRef.current)}>To LinkREF</button>
             <h5>Nama pacar : {namaPacar}</h5>
             <h1>Saya memiliki : {pacarSaya} pacar</h1>
             <button onClick={() => setPacarSaya((prev) => prev + 1)}>tambah pacar</button>
             <button onClick={() => setPacarSaya((prev) => prev - 1)}>kurangi pacar</button>
-
-
-
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
             <Profile profileVal={getProfileVal}/>
             <Introduction name="Hilman" />
             <Introduction name="Jajang" />
@@ -79,6 +53,13 @@ function HomePages(){
             <button onClick={() => changeProfile()}>Ubah Profile</button>
             <br/>
             <button onClick={() => navigate('/about')}>About</button>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus autem quo! Repellendus quia nisi ratione neque aliquam quisquam dolorum rem vel minima eligendi reiciendis ipsam iure assumenda ab inventore rerum, animi doloremque tenetur consequuntur error omnis ut! Tenetur, deleniti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus autem quo! Repellendus quia nisi ratione neque aliquam quisquam dolorum rem vel minima eligendi reiciendis ipsam iure assumenda ab inventore rerum, animi doloremque tenetur consequuntur error omnis ut! Tenetur, deleniti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus autem quo! Repellendus quia nisi ratione neque aliquam quisquam dolorum rem vel minima eligendi reiciendis ipsam iure assumenda ab inventore rerum, animi doloremque tenetur consequuntur error omnis ut! Tenetur, deleniti!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus autem quo! Repellendus quia nisi ratione neque aliquam quisquam dolorum rem vel minima eligendi reiciendis ipsam iure assumenda ab inventore rerum, animi doloremque tenetur consequuntur error omnis ut! Tenetur, deleniti!</p>
+            <a href="#" className="kontol" ref={linkRef}>
+                LinkREF
+            </a>
         </>
     );
 }
